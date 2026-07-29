462aeaf8f855642684716dafa15e7b0527cb33a2
MatheusLima20 <36931078+MatheusLima20@users.noreply.github.com>
2026-07-29 19:36:39 -0300

    refactor (finance): move files and folders to your module.

---
+++ src/modules/farmora/finance/transaction/mappers/transaction.mapper.ts
@@ -0,0 +1,42 @@
+import { ResponseTransactionDTO } from "../transaction/dtos/transaction-response.dto";
+import { UpdateTransactionResponseDTO } from "../transaction/dtos/update-transaction.dto";
+import { TransactionProps } from "../transaction/entities/transaction.props";
+
+export const TransactionMapper = {
+    toResponseDTO: (transaction: TransactionProps): ResponseTransactionDTO => {
+        return {
+            uid: transaction.uid,
+            platformUID: transaction.platformUID,
+            categoryUID: transaction.categoryUID,
+            type: transaction.type,
+            description: transaction.description,
+            amount: transaction.amount,
+            occurredAt: transaction.occurredAt,
+            source: transaction.source,
+            sourceUID: transaction.sourceUID,
+            notes: transaction.notes,
+            createdBy: transaction.createdBy,
+            updatedBy: transaction.updatedBy,
+            createdAt: transaction.createdAt,
+            updatedAt: transaction.updatedAt,
+        };
+    },
+
+    toResponseDTOList: (transactions: TransactionProps[]): ResponseTransactionDTO[] => {
+        return transactions.map(TransactionMapper.toResponseDTO);
+    },
+
+    toUpdatedResponseDTO: (transaction: TransactionProps): UpdateTransactionResponseDTO => {
+        return {
+            uid: transaction.uid,
+            description: transaction.description,
+            type: transaction.type,
+            amount: transaction.amount,
+            notes: transaction.notes,
+            categoryUID: transaction.categoryUID,
+            occurredAt: transaction.occurredAt,
+            updatedBy: transaction.updatedBy,
+            updatedAt: transaction.updatedAt,
+        };
+    },
+};
